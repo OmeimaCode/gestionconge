95c7f86c934713fc941c9b14b51ab55cd5b45d63
ohraichi <ohraichi@gmail.com>
2022-09-10 22:47:16 +0100

    Hello World 2

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className='h-screen w-screen flex flex-col justify-center items-center'>
-        <h1 className='text-4xl font-bold animate-pulse text-blue-600'>Hello World</h1>
+        <h1 className='text-4xl font-bold animate-pulse text-blue-600'>Hello World 2</h1>
       </div>
     </div>
   )
